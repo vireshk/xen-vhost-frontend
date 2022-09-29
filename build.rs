71e7502ffaebe9d14beee320fd727012d6db5f1f
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-09-29 15:24:12 -0600

    Get rid of build.rs
    It is no longer required to link with the libraries, hence
    removing build.rs
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=/opt/xen-arm/dist/install/usr/lib/");
-    println!("cargo:rustc-link-lib=xenstore");
-    println!("cargo:rustc-link-lib=xentoolcore");
-    println!("cargo:rustc-link-lib=xentoollog");
-    println!("cargo:rustc-link-lib=xencall");
-    println!("cargo:rustc-link-lib=xenctrl");
-}
